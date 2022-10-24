5c58b3a7f50becba2465b95118c24dddb303dbde
Martin Christov <martin.christov@gmail.com>
2022-10-24 10:46:58 +0300

    page view proper

--- components/page.js
+++ components/page.js
@@ -12,19 +12,16 @@ pagesJson.forEach((verse, i) => {
   }
   pages[pagesJson[i].pageNumber].push({...verse, text: surah.verse[`verse${verse.verseNumber}`]})
 })
-console.log(pages)
 const startI = 64337 // do 64433; ot 64467 (add 34)
 const Page = ({ p = 2 }) => {
   let text = ''
   let pageContentLength = 0
-  console.log(pages[p])
   pages[p].forEach((verse) => {
-    console.log(verse.text.split(' ').filter(v => v.length > 1))
     pageContentLength += verse.text.split(' ').filter(v => v.length > 0).length + 1
   })
   for(let i = startI; i < startI + pageContentLength; i += 1){
     let index = i
-    if(index > 64433) index += 34
+    if(index > 64432) index += 34
     text = `${text}${String.fromCharCode(index)}`
   }
   
