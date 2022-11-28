45e43028b5fb2d0d0d3eb92ea9ce00f0296a10a8
Martin Christov <martin.christov@gmail.com>
2022-11-28 14:31:35 +0300

    refactor, cleanup

--- components/safha.js
+++ components/safha.js
@@ -1,32 +1,14 @@
-// import versesRaw from 'quranjson/source/pages.json'
-import page2surah from '../assets/page2surah.json'
-// import quran from '../assets/quran.json'
-// import surah2 from '../assets/surah-2.json'
-import humps from 'humps'
 import classNames from 'classnames'
 import { useEffect, useState } from 'react'
-// console.log(surah2)
 
-// const versesJson = humps.camelizeKeys(versesRaw)
-
-const startI = 64337 // do 64433; ot 64467 (add 34)
 const Safha = ({ p }) => {
-  const [content, setContent] = useState([])
+  const [pageAyat, setPageAyat] = useState()
   
   useEffect(() => {
     const renderText = async () => {
       const ayatR = await fetch(`https://api.quran.com/api/v4/verses/by_page/${p}?language=en&words=true&word_fields=code_v2`)
       const ayat = await ayatR.json()
-
-      const content = []
-      ayat.verses.forEach(verse => {
-        content.push(<span className="aya">{verse.words.map(word => <><span className="kalima" key={word.id}>{word.code_v2}</span> </>)}</span>)
-      })
-      console.log(p, ayat)
-      if(p === 2){
-        content.unshift((<span key="bismillah" className="bismillah">ﱁ ﱂ ﱃ ﱄ</span>))
-      }
-      setContent(content)
+      setPageAyat(ayat)
     }
     renderText()
   }, [])
@@ -36,7 +18,10 @@ const Safha = ({ p }) => {
       <div className="content">
         <div className="inner">
           <p>
-            {content}
+            {p === 2 && <span key="bismillah" className="bismillah">ﱁ ﱂ ﱃ ﱄ</span>}
+            {pageAyat?.verses.map(verse =>
+              <span className="aya" key={verse.id}>{verse.words.map(word => <><span className="kalima" key={word.id}>{word.code_v2}</span> </>)}</span>
+            )}
           </p>
         </div>
       </div>
