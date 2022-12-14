54f1a72ae832e824183462d767117cd44d711117
Martin Christov <martin.christov@gmail.com>
2022-12-14 19:43:09 +0200

    nav upgrades

--- components/nav.js
+++ components/nav.js
@@ -1,21 +1,43 @@
 import { Modal } from "antd"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import surat from '../assets/surat.json'
+import page2sura from '../assets/page2surah.json'
 
-const Nav = ({ page = 1, setPage, pageh }) => {
+const pageh = 860;
+const marginY = 48
+
+const Nav = ({ initers, setIniters }) => {
+  const [page, setPage] = useState(1)
   const [suraModalVisible, setSuraModalVisible] = useState(false)
+  const [currentSura, setCurrentSura] = useState('الفتحة')
   const handlePageClick = () => {
     const inp = prompt('Jump to page', page)
     if(inp != null){
       window.scrollTo({ top: document.getElementsByClassName('page')[Number(inp)].offsetTop - 50 })
-      
     }
   }
+  useEffect(() => {
+    document.addEventListener('scroll', () => {
+      const pageYPos = Math.floor((window.scrollY + marginY) / pageh)
+      if(pageYPos + 1 !== page){
+        setPage(pageYPos + 1)
+        console.log(pageYPos, page2sura[pageYPos + 1])
+        setCurrentSura(surat.chapters[page2sura[pageYPos + 1] - 1].name_arabic)
+        if(!initers[pageYPos] || !initers[pageYPos + 1] || !initers[pageYPos + 2]){
+          const cp = [...initers]
+          cp[pageYPos] = true
+          cp[pageYPos + 1] = true
+          cp[pageYPos + 2] = true
+          setIniters(cp)
+        }
+      }
+    })
+  }, [])
   return (
     <>
       <nav>
         <div className="page-contain">
-          <div className="surah caption" onClick={() => setSuraModalVisible(true)}>الفتحة</div>
+          <div className="surah caption" onClick={() => setSuraModalVisible(true)}>{currentSura}</div>
           <div className="page" onClick={handlePageClick}>{ConvertToArabicNumbers(page)}</div>
           <div className="juz caption">الجزء ١</div>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -3,31 +3,14 @@ import Safha from '../components/safha'
 import Nav from '../components/nav'
 
 export default function Home() {
-  const [page, setPage] = useState(1)
   const [initers, setIniters] = useState([true, true, true, true])
   const pages = []
-  const pageh = 860;
   for(let i = 1; i < 604; i += 1){
     pages.push(<Safha p={i} key={i} init={initers[i - 1]} />)
   }
-  useEffect(() => {
-    document.addEventListener('scroll', () => {
-      const pageYPos = Math.floor(window.scrollY / pageh)
-      if(pageYPos !== page){
-        setPage(pageYPos + 1)
-        if(!initers[pageYPos] || !initers[pageYPos + 1] || !initers[pageYPos + 2]){
-          const cp = [...initers]
-          cp[pageYPos] = true
-          cp[pageYPos + 1] = true
-          cp[pageYPos + 2] = true
-          setIniters(cp)
-        }
-      }
-    })
-  }, [])
   return (
     <div>
-      <Nav {...{ page, setPage, pageh}} />
+      <Nav {...{ initers, setIniters }} />
       {pages}
     </div>
   )
