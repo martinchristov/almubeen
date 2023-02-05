a3dd405e85fb827200f27338c926a8559d2cbcc8
Martin Christov <martin.christov@gmail.com>
2023-02-05 11:18:45 +0200

    fix smartlook client

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,6 @@ import AyaTranslations from '../components/translations'
 import mixpanel from 'mixpanel-browser';
 import smartlookClient from 'smartlook-client'
 mixpanel.init('c8410392727607e9cb045c0145343357', {debug: true});
-smartlookClient.init('7e0e68377f7697cb8fb21a46ad6a70dd89b9f982', { region: 'eu' })
 
 export default function Home() {
   const [initers, setIniters] = useState([true, true, true, true])
@@ -22,6 +21,9 @@ export default function Home() {
       setMarkAya(null)
     }, 15000)
   }
+  useEffect(() => {
+    smartlookClient.init('7e0e68377f7697cb8fb21a46ad6a70dd89b9f982', { region: 'eu' })
+  }, [])
   return (
     <div>
       <Nav {...{ initers, setIniters, highlightAya }} />
