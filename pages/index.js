f28097fea700714a934229bd1d868a12ba253187
Martin Christov <martin.christov@gmail.com>
2023-02-05 11:16:45 +0200

    add smartlook

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,9 @@ import Nav from '../components/nav'
 import { Modal } from 'antd'
 import AyaTranslations from '../components/translations'
 import mixpanel from 'mixpanel-browser';
+import smartlookClient from 'smartlook-client'
 mixpanel.init('c8410392727607e9cb045c0145343357', {debug: true});
+smartlookClient.init('7e0e68377f7697cb8fb21a46ad6a70dd89b9f982', { region: 'eu' })
 
 export default function Home() {
   const [initers, setIniters] = useState([true, true, true, true])
