6e14b77706f434f6aa74b6af719bc68b9db14c2b
Martin Christov <martin.christov@gmail.com>
2023-02-24 19:43:31 +0200

    title

--- pages/index.js
+++ pages/index.js
@@ -31,6 +31,7 @@ export default function Home() {
           name='viewport' 
           content='width=device-width' 
       />
+      <title>Al Mubeen | المبين</title>
     </Head>
       <ConfigProvider
         theme={{
