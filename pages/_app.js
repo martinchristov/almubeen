d26a29a0831c54ca617a3cdf2597a56ccf0a9a9d
Martin Christov <martin.christov@gmail.com>
2023-03-13 16:25:52 +0200

    implement next-auth with email and supabse

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
+import { SessionProvider } from "next-auth/react"
 import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <SessionProvider session={pageProps.session}>
+    <Component {...pageProps} />
+  </SessionProvider>
+  )
 }
 
 export default MyApp
