9ff32727739ac042c265709c8b5241deeb142ff2
Martin Christov <martin.christov@gmail.com>
2023-03-27 13:14:45 +0300

    api collections

--- pages/api/collections/index.js
+++ pages/api/collections/index.js
@@ -10,7 +10,8 @@ export default async function handler(req, res) {
     return;
   }
 
-  const { supabaseAccessToken } = session
+  const { supabaseAccessToken, user, ...props } = session
+  // console.log('has access token', supabaseAccessToken)
 
   const supabase = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL,
@@ -23,11 +24,42 @@ export default async function handler(req, res) {
       },
     }
   )
-  // Now you can query with RLS enabled.
-  const { data, error } = await supabase.from("collections").select("*")
+
+  if(req.method === 'POST'){
+    if(!req.body.key){
+      return res.json({ message: 'Error', error: 'No key' })
+    }
+    console.log(session)
+    const { data, error } = await supabase
+      .from('collections')
+      .select('id')
+      .eq('uid', session.uid)
+      .eq('title', 'Bookmarks')
+    
+    if(!error && data.length > 0){
+      await supabase
+        .from('ayat')
+        .insert({ key: req.body.key, coll_id: data[0].id })
+      
+    }
+    return res.json({
+      message: 'Success',
+      // error
+    })
+  }
+
+  const { data, error } = await supabase.from("collections").select("*").eq('uid', session.uid)
+  const resp = []
+  for(let item of data){
+    const keys = await supabase.from('ayat').select('key').eq('coll_id', item.id)
+    resp.push({...item, keys: keys.data.map(it => it.key) })
+  }
 
   return res.json({
     message: 'Success',
-    data, error
+    data: {
+      collections: resp,
+    },
+    error
   })
 }
