2d3c5bc03581a45947f7de79100ab1d99efe2a47
Umar Begg <umar97@hotmail.co.uk>
2024-02-27 13:46:35 +0000

    added all words with the same parent_id to response

--- pages/api/word.js
+++ pages/api/word.js
@@ -6,15 +6,35 @@ const db = new sqlite3.Database(dbPath)
 
 export default async function handler(req, res) {
   const { s } = req.query
-  if (req.method === 'GET') {
-    const query = 'SELECT * FROM DICTIONARY WHERE word=? AND is_root=1'
 
-    db.all(query, [s], (err, rows) => {
-      if (err) {
-        res.status(500).json({ error: err.message })
+  if (req.method === 'GET') {
+    const parentQuery =
+      'SELECT parent_id FROM DICTIONARY WHERE word=? AND is_root=1'
+
+    db.get(parentQuery, [s], (parentErr, parentRow) => {
+      if (parentErr) {
+        res.status(500).json({ error: parentErr.message })
         return
       }
-      res.json(rows)
+
+      if (!parentRow) {
+        res.status(404).json({ error: 'Word not found with is_root=1' })
+        return
+      }
+
+      const parentId = parentRow.parent_id
+
+      const resultQuery =
+        'SELECT * FROM DICTIONARY WHERE (word=? AND is_root=1) OR (parent_id=? AND is_root=0)'
+
+      db.all(resultQuery, [s, parentId], (resultErr, rows) => {
+        if (resultErr) {
+          res.status(500).json({ error: resultErr.message })
+          return
+        }
+
+        res.json(rows)
+      })
     })
   } else {
     res.status(405).json({ error: 'Method Not Allowed' })
