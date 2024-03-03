314d3b30b18d2f11cfe78c8ee163319b01f660ee
UmarBegg <umar97@hotmail.co.uk>
2024-03-03 16:23:07 +0000

    fix

--- components/hans-wehr-display.js
+++ components/hans-wehr-display.js
@@ -8,10 +8,11 @@ function HansWehrDisplay({ arabicRoot, open }) {
     const fetchData = async () => {
       try {
         const response = await fetch(
+          // currently pointing at localhost, needs fixing
           `http://localhost:3000/api/word/?s=${arabicRoot}`
         )
         const newData = await response.json()
-        setData(newData)
+        if (Array.isArray(newData) && newData.length > 0) setData(newData)
       } catch (error) {
         console.error('Error fetching data:', error)
       }
@@ -24,12 +25,12 @@ function HansWehrDisplay({ arabicRoot, open }) {
     <Modal className="hans-wehr-modal" open={open} footer={null}>
       <h1>Hans Wehr Dictionary</h1>
       {data.map((word) => (
-        <>
+        <div key={word.id}>
           <h2>Word: {word.word}</h2>
-          <p key={word.id}>
-            <strong>Definition:</strong> {word.definition}{' '}
+          <p>
+            <strong>Definition:</strong> {word.definition}
           </p>
-        </>
+        </div>
       ))}
     </Modal>
   )
